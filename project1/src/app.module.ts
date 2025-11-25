49af1932ac6f0c3984b3ebdec6af1af17a33a647
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-11-25 22:58:48 +0600

    add middileware

--- project1/src/app.module.ts
+++ project1/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ExceptionController } from './exception/exception.controller';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController, ExceptionController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
